ee1b50449e46061d108051f32618fddb3a21f178
Ignacio <ignaciocieza@hotmail.com>
2018-11-28 14:17:16 -0300

    Agrego nuevos archivos

---
+++ programacion_1_laboratorio_1-master/programacion_1_laboratorio_1-master/Clase_21/Clase_18/Employee.h
@@ -0,0 +1,41 @@
+/*
+    utest example : Unit test examples.
+    Copyright (C) <2016>  <Mauricio Davila>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _EMPLOYEE_H
+#define _EMPLOYEE_H
+struct
+{
+    char name[128];
+    char email[128];
+}typedef Employee;
+
+
+Employee* employee_new(void);
+Employee* employee_newParametros(char* name, char* email);
+void employee_delete(Employee* this);
+int employee_setName(Employee* this, char* name);
+int employee_getName(Employee* this, char* name);
+int employee_setEmail(Employee* this, char* email);
+int employee_getEmail(Employee* this, char* email);
+int employee_compare(Employee* pEmployeeA,Employee* pEmployeeB);
+void employee_print(Employee* this);
+int employee_compareName(Employee* pEmployeeA,Employee* pEmployeeB);
+#endif // _EMPLOYEE_H
+
+
+
---
+++ programacion_1_laboratorio_1-master/programacion_1_laboratorio_1-master/Clase_21/Clase_18/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+#include "parser.h"
+
+/****************************************************
+    Menu:
+        1. Parse del archivo data.csv
+        2. Listar Empleados
+        3. Ordenar por nombre
+        4. Agregar un elemento
+        5. Elimina un elemento
+        6. Listar Empleados (Desde/Hasta)
+*****************************************************/
+
+
+int main()
+{
+    ArrayList* listaEmpleados = al_newArrayList();
+
+    //FILE* pFile;
+    parserEmployee("destinatarios.csv",listaEmpleados);
+
+    int j;
+    for(j=0; al_len(listaEmpleados); j++)//al_len(blackList);
+    {
+        Employee* d = al_get(listaEmpleados, j); //al_get(blackList, j);
+
+        int index = al_indexOfElement(listaEmpleados, d, comparoDest);//al_indexOfElement(destinatarios, d, comparoDest);
+
+        al_remove(listaEmpleados, index);//al_remove(destinatarios, index);
+    }
+
+
+    return 0;
+}
